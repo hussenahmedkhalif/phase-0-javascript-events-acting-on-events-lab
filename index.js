e477ed343347fc91213e6b04789e82aa8dbbb15b
Farhan-rashid10 <farkaf07@gmail.com>
2024-05-23 10:56:42 +0300

    after assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dorger = document.getElementById("dodger")
+document.addEventListener("keydown", function(e){
+    if(e.key ==="ArrowLeft"){
+        const newleft =dorger.style.left.replace("px", "");
+        const leftint =parseInt(newleft);
+        console.log(leftint)
+            if(leftint >=0){
+                dorger.style.left = `${leftint-1}px`
+
+            }
+            else{
+                alert("game over")
+            }
+       
+
+    }else if(e.key ==="ArrowRight"){
+        const newleft =dorger.style.left.replace("px", "");
+        const leftint =parseInt(newleft);
+        if(leftint <=360){
+            dorger.style.left = `${leftint+1}px`
+
+        }
+
+    }else{
+        alert("Click the right key")
+    }
+    
+ })
